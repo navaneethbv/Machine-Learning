c4747444443e0fe2ddc0412f2ef7ea22c82bfc8b
unknown <navaneethbv@gmail.com>
2015-09-26 21:39:48 -0500

    Adding Assignment 3

---
+++ Post Pruning and Accuracy for Decision Trees using R/Source Code/assignment3_2.R
@@ -0,0 +1,21 @@
+library(rpart)
+data("kyphosis")
+str(kyphosis)
+floor_val<-floor(0.9*nrow(kyphosis))
+train<-sample(nrow(kyphosis),size=floor_val)
+trainingData<-kyphosis[train,]
+testData<-kyphosis[-train,]
+kyphosis_fit<-rpart(Kyphosis~Age+Number+Start,parms=list(split='information'), minsplit=2, minbucket=1,data=trainingData,method='class')
+par(mar = rep(0.1, 4))
+plot(kyphosis_fit)
+text(kyphosis_fit)
+summary(kyphosis_fit)
+CPVal<-kyphosis_fit$cptable[which.min(kyphosis_fit$cptable[,"xerror"]),"CP"]
+CPVal
+pruned_fit<-prune(kyphosis_fit,cp=as.numeric(CPVal))
+out<-predict(pruned_fit,newdata=testData,type="vector")
+tab <- table(out, testData$Kyphosis)
+tab
+accuracy<-sum(diag(tab))/sum(tab)
+accuracyPercentage<-accuracy*100
+accuracyPercentage
---
+++ Post Pruning and Accuracy for Decision Trees using R/Source Code/assignment3_3.R
@@ -0,0 +1,21 @@
+library(rpart)
+data("solder")
+str(solder)
+floor_val<-floor(0.8*nrow(solder))
+train<-sample(nrow(solder),size=floor_val)
+trainingData<-solder[train,]
+testData<-solder[-train,]
+solder_fit<-rpart(Solder~.,parms=list(split='information'), minsplit=2, minbucket=1,data=trainingData,method='class')
+par(mar = rep(0.1, 4))
+plot(solder_fit)
+text(solder_fit)
+summary(solder_fit)
+CPVal<-solder_fit$cptable[which.min(solder_fit$cptable[,"xerror"]),"CP"]
+CPVal
+pruned_fit<-prune(solder_fit,cp=as.numeric(CPVal))
+out<-predict(pruned_fit,newdata=testData,type="vector")
+tab <- table(out, testData$Solder)
+tab
+accuracy<-sum(diag(tab))/sum(tab)
+accuracyPercentage<-accuracy*100
+accuracyPercentage
