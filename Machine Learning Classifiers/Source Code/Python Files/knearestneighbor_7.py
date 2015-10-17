da73e0f6a90acef34087a83607819ae64727770e
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2015-10-17 03:08:38 -0500

    Update knearestneighbor_7.py

--- Machine Learning Classifiers/Source Code/Python Files/knearestneighbor_7.py
+++ Machine Learning Classifiers/Source Code/Python Files/knearestneighbor_7.py
@@ -10,7 +10,7 @@ def loadDataset(filename, split, trainingSet=[] , testSet=[]):
 	with open(filename, 'r') as csvfile:
 	    lines = csv.reader(csvfile)
 	    dataset = list(lines)
-	    for x in range(len(dataset)-1):
+	    for x in range(len(dataset)-1): 
 	        for y in range(4):
 	            dataset[x][y] = float(dataset[x][y])
 	        if random.random() < split:
