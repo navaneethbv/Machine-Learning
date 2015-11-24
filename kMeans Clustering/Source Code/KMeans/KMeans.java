19fa4d2c63635aca0d924cdd37e1d98d84bf96da
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2015-11-23 22:30:37 -0600

    Update KMeans.java

--- kMeans Clustering/Source Code/KMeans/KMeans.java
+++ kMeans Clustering/Source Code/KMeans/KMeans.java
@@ -6,7 +6,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Collections; 
 import java.util.Iterator;
 import java.util.LinkedList;
 
