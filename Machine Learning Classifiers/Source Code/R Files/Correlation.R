aee36e6a25ee0b705ff63ee1c11124196e2b5176
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2016-06-07 23:57:59 -0500

    Update Correlation.R

--- Machine Learning Classifiers/Source Code/R Files/Correlation.R
+++ Machine Learning Classifiers/Source Code/R Files/Correlation.R
@@ -1,4 +1,4 @@
-args <- commandArgs(TRUE)
+args <- commandArgs(TRUE) 
 fileName <- args[1]
 diabetes_data<-read.csv(fileName,header = FALSE)
 paste("The correlation between attribute1 and the class variable is: ",cor(diabetes_data$V1,diabetes_data$V9),sep="")
