56b96cd9260a671b9b19f442235d2bbf5ddf3160
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2016-06-22 00:02:29 -0500

    Update testScript.sh

--- Machine-Learning-Classifiers-Comparison/Source Code/testScript.sh
+++ Machine-Learning-Classifiers-Comparison/Source Code/testScript.sh
@@ -1,6 +1,6 @@
 declare -a urls=('http://www.utdallas.edu/~axn112530/cs6375/creditset.csv' 'http://www.ats.ucla.edu/stat/data/binary.csv' 'http://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wpbc.data' 'http://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data' 'http://archive.ics.uci.edu/ml/machine-learning-databases/ionosphere/ionosphere.data');
 declare -a headers=('T' 'T' 'F' 'F' 'F');
-declare -a classPosition=(6 1 2 2 35); 
+declare -a classPosition=(6 1 2 2 35);  
 
 echo "\nEnter dataset between 1-5 or enter 0 to run all at once"
 read choice
