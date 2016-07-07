7f939fc336b3a74e7598c624dc9edd852529fc24
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2016-07-07 18:24:39 -0500

    Update C_Game.java

--- Assignment_1_Craps-Game/Code/C_Game.java
+++ Assignment_1_Craps-Game/Code/C_Game.java
@@ -5,7 +5,7 @@ public class C_Game
 	public static void main(String[] args) 
 	{
 		System.out.println("-----Let's Play the GAME of CRAPS-----");
-		System.out.println("Starting Balance is $1000");
+		System.out.println("Starting Balance is $1000"); 
 		GameOfCrap goc = new GameOfCrap(); 
 		goc.startGame();
 	}
