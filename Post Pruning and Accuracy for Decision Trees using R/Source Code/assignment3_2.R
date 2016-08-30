b5f8d37d0eb68345c9b00cfe204fb5d4371c93cb
Navaneeth Rao <navaneethbv@users.noreply.github.com>
2016-08-30 10:22:52 -0500

    Update assignment3_2.R

--- Post Pruning and Accuracy for Decision Trees using R/Source Code/assignment3_2.R
+++ Post Pruning and Accuracy for Decision Trees using R/Source Code/assignment3_2.R
@@ -7,7 +7,7 @@ trainingData<-kyphosis[train,]
 testData<-kyphosis[-train,] 
 kyphosis_fit<-rpart(Kyphosis~Age+Number+Start,parms=list(split='information'), minsplit=2, minbucket=1,data=trainingData,method='class')
 par(mar = rep(0.1, 4))
-plot(kyphosis_fit)
+plot(kyphosis_fit) 
 text(kyphosis_fit) 
 summary(kyphosis_fit)
 CPVal<-kyphosis_fit$cptable[which.min(kyphosis_fit$cptable[,"xerror"]),"CP"]
